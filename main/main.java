6334cc2c3404bd5fb6c98cd859becbfbe7bb4b71
JoachimRay <131645680+JoachimRay@users.noreply.github.com>
2025-11-17 09:51:21 +0800

    Transactions integrated
    Added the option to be able to add transactions

--- main/main.java
+++ main/main.java
@@ -16,6 +16,9 @@ public class main {
 		String url = "jdbc:sqlite:Database/Wallet.db"; 
 		String Checkaccount = "SELECT account_id, account FROM wallet"; 
 		String sql = "INSERT OR REPLACE INTO wallet (account_id, account, account_balance) VALUES(?,?,?)";
+		String Addtransaction = "INSERT OR REPLACE into transactions(tranx_id, account_id, category_id, tranx_type_id, tranx_amount, tranx_time_date) VALUES (?,?,?,?,?,?)";  
+		String DisplayCategories = "SELECT category_id, category FROM categories";
+		String DisplayTransactionTypes = "SELECT tranx_type_id, tranx_type FROM transaction_type";
 
 		Scanner input = new Scanner(System.in); 
 
@@ -23,17 +26,21 @@ public class main {
 		System.out.println("Welcome to Finance Tracker!"); 
 
 		int choices = 0; 
+		double tranx_id = 0; 
 
-		while(choices != 3)
+		while(choices != 4)
 		{
 			choices = 0; 
-		System.out.println("\n1.)Accounts\n2.)Add Account\n3.)Exit.."); 
+		System.out.println("\n1.)Accounts\n2.)Add Account\n3.)Add Transaction\n4.)Exit.."); 
 		System.out.print("\nEnter Choice: "); 
 		choices = input.nextInt(); 
 
 		switch(choices)
 		{
 			case 1: 
+			{
+
+			
 			try {
 				
 				Class.forName("org.sqlite.JDBC"); 
@@ -61,10 +68,12 @@ public class main {
 			}
 
 			break; 
+		  }
 
 
 
 			case 2: 
+			{ 
 
 			System.out.println("Enter new account details: "); 
 			System.out.println("Account ID: "); 
@@ -73,6 +82,8 @@ public class main {
 			String new_account = input.next(); 
 			System.out.println("Initial Balance: ");
 			double new_balance = input.nextDouble(); 
+
+			
 			
 		try {
 			// Ensure the JDBC driver is loaded when running with `java -cp ...`
@@ -99,10 +110,104 @@ public class main {
 		}
 
 		break; 
+	}
 
-		case 3:{ 
+		case 3: 
+		{
+			System.out.println("Categories: "); 
+			try{ 
+				Class.forName("org.sqlite.JDBC"); 
+
+				try(Connection conn = DriverManager.getConnection(url);
+				PreparedStatement ps = conn.prepareStatement(DisplayCategories)){ 
+					try(ResultSet rs = ps.executeQuery())
+					{
+						while(rs.next())
+						{
+							int cat_id = rs.getInt("category_id"); 
+							String cat_name = rs.getString("category"); 
+							System.out.println("Category ID: " + cat_id + " Category Name: " + cat_name + "\n");
+						}
+					}
+
+				};
+			}catch (ClassNotFoundException e) {
+			System.out.println("SQLite JDBC driver not found on classpath; skipping DB operations.");
+		} catch (SQLException e) {
+			System.out.println("Error " + e.getMessage());
+		}
+
+		System.out.println("Transaction Types: "); 
+
+		try{ 
+				Class.forName("org.sqlite.JDBC"); 
+
+				try(Connection conn = DriverManager.getConnection(url);
+				PreparedStatement ts = conn.prepareStatement(DisplayTransactionTypes)){ 
+					try(ResultSet rs = ts.executeQuery())
+					{
+						while(rs.next())
+						{
+							int T_id = rs.getInt("tranx_type_id"); 
+							String T_name = rs.getString("tranx_type"); 
+							System.out.println("Transaction Type ID: " + T_id + " Transaction Type Name: " + T_name + "\n");
+						}
+					}
+
+				};
+			}catch (ClassNotFoundException e) {
+			System.out.println("SQLite JDBC driver not found on classpath; skipping DB operations.");
+		} catch (SQLException e) {
+			System.out.println("Error inserting data! " + e.getMessage());
+		}
+		
+		System.out.println("---------------------------------------------"); 
+		System.out.println("Enter Transaction Details: "); 
+		System.out.print("Account ID: "); 
+		double acc_id = input.nextDouble(); 
+		System.out.print("Category ID: ");
+		double Category_id = input.nextDouble(); 
+		System.out.print("Transaction Type ID: "); 
+		double T_T_id = input.nextDouble(); 
+		System.out.print("Transaction Amount: "); 
+		double T_amount = input.nextDouble(); 
+		System.out.print("Transaction Date and Time (YYYY-MM-DD HH:MM:SS): "); 
+		String T_date_time = input.next(); 
+
+		try{ 
+
+			Class.forName("org.sqlite.JDBC"); 
+
+			try(Connection conn = DriverManager.getConnection(url); 
+			PreparedStatement pstmt = conn.prepareStatement(Addtransaction)){
+
+				tranx_id++; 
+
+				pstmt.setDouble(1,tranx_id); 
+				pstmt.setDouble(2, acc_id);
+				pstmt.setDouble(3, Category_id); 
+				pstmt.setDouble(4, T_T_id); 
+				pstmt.setDouble(5, T_amount); 
+				pstmt.setString(6, T_date_time);
+				pstmt.executeUpdate(); 
+
+				System.out.println("Transaction added successfully!"); 
+			}
+
+		}catch(ClassNotFoundException e) {
+			System.out.println("SQLite JDBC driver not found on classpath; skipping DB operations.");
+		} catch (SQLException e) {
+			System.out.println("Error inserting data! " + e.getMessage());
+		}
+		break; 
+
+
+		}
+
+		case 4:{ 
 			System.out.println("Exiting...");
 			System.exit(0);
+			break; 
 		}
 
 		default: 
